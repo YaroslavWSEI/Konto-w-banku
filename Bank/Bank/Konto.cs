3ac21b3966b2a8ec8297edff401be66327b5222a
Yaroslav Furmanov <yaroslav.furmanov@microsoft.wsei.edu.pl>
2025-03-23 19:02:22 +0100

    Krok 1 1-10

---
+++ Bank/Bank/Konto.cs
@@ -0,0 +1,39 @@
+﻿namespace Bank
+{
+    public class Konto
+    {
+        private string klient;
+        private decimal bilans; 
+        private bool zablokowane = false;
+        public Konto(string klient, decimal bilansNaStart = 0)
+        {
+            this.klient = klient;
+            this.bilans = bilansNaStart;
+        }
+        public string Nazwa => klient;
+        public decimal Bilans => bilans;
+        public bool Zablokowane => zablokowane;
+        public void Wplata(decimal kwota)
+        {
+            if (zablokowane)
+            {
+                throw new Exception("Konto zablokowane");
+            }
+            if (kwota <= 0)
+            {
+                throw new Exception("Kwota musi być dodatnia");
+            }
+            bilans += kwota;
+        }
+        public void BlokujKonto()
+        {
+            zablokowane = true;
+        }
+        public void OdblokujKonto()
+        {
+            zablokowane = false;
+        }
+
+
+    }
+}
