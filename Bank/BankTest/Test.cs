94639654c26b1023e1b2052e3a348690358e8919
Yaroslav Furmanov <yaroslav.furmanov@microsoft.wsei.edu.pl>
2025-03-23 19:53:43 +0100

    Krok 1 11-13

---
+++ Bank/BankTest/Test.cs
@@ -0,0 +1,34 @@
+﻿using Bank;
+namespace BankTest
+{
+    [TestClass]
+    public sealed class Test
+    {
+        [TestMethod]
+
+        //test wpłaty
+        public void TestWplata()
+        {
+            var konto = new Konto("Yaroslav Furmanov", 100);
+            konto.Wplata(50);
+            Assert.AreEqual(150, konto.Bilans);
+        }
+        [TestMethod]
+        //test wyplaty
+        public void TestWyplata()
+        {
+            var konto = new Konto("Yaroslav Furmanov", 100);
+            konto.Wyplata(50);
+            Assert.AreEqual(50, konto.Bilans);
+        }
+        //test wpłaty z ujemną kwotą
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Kwota musi być dodatnia")]
+        public void TestWplataNegativeAmount()
+        {
+            var konto = new Konto("Yaroslav Furmanov", 100);
+            konto.Wplata(-50);
+        }
+       
+    }
+}
--- Bank/KontoPlus/KontoPlus.cs
+++ Bank/KontoPlus/KontoPlus.cs
@@ -3,16 +3,19 @@
     public class Konto
     {
         private string klient;
-        private decimal bilans; 
+        protected decimal bilans; // Изменено на protected
         private bool zablokowane = false;
+
         public Konto(string klient, decimal bilansNaStart = 0)
         {
             this.klient = klient;
             this.bilans = bilansNaStart;
         }
+
         public string Nazwa => klient;
         public decimal Bilans => bilans;
         public bool Zablokowane => zablokowane;
+
         public void Wplata(decimal kwota)
         {
             if (zablokowane)
@@ -25,31 +28,15 @@
             }
             bilans += kwota;
         }
-        public void Wyplata(decimal kwota)
-        {
-            if (zablokowane)
-            {
-                throw new Exception("Konto zablokowane");
-            }
-            if (kwota <= 0)
-            {
-                throw new Exception("Kwota musi być dodatnia");
-            }
-            if (bilans < kwota)
-            {
-                throw new Exception("Brak środków na koncie");
-            }
-            bilans -= kwota;
-        }
+
         public void BlokujKonto()
         {
             zablokowane = true;
         }
+
         public void OdblokujKonto()
         {
             zablokowane = false;
         }
-
-
     }
 }
